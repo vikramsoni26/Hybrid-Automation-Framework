3038b32380e4c2483ef5eb91742f9a490ac6c446
Vikram Soni <vikramsoni751@gmail.com>
2026-04-23 20:25:48 +0530

    Initial commit - Selenium Framework Day 1 & 2

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+public class LoginTest extends BaseTest  {
+	
+	@Test
+	public void openWebsiteTest() {
+		System.out.println("Website Open sucessfully");
+	}
+
+}
