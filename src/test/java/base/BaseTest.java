88a351e62511f9955302074979406205bf5c7014
Vikram Soni <vikramsoni751@gmail.com>
2026-04-24 17:58:37 +0530

    day 2
    Day 2 - Added TestNG setup, multiple test cases and WebDriverManager integration"

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -5,12 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 	
 	public WebDriver driver;
 	
 	@BeforeMethod
 	public void setup() {
+		WebDriverManager.chromedriver().setup(); // auto driver
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.saucedemo.com/");
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -10,5 +10,17 @@ public class LoginTest extends BaseTest  {
 	public void openWebsiteTest() {
 		System.out.println("Website Open sucessfully");
 	}
+	
+	@Test
+	public void verifyTitle() {
+		String title = driver.getTitle();
+		System.out.println(title);
+	}
+	
+	@Test
+	public void verifyURL() {
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+	}
 
 }
